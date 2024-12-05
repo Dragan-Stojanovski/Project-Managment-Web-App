ce1693d305bd11bc5c0c4fb0b470e49861477cec
Dragan-Stojanovski <100153691+Dragan-Stojanovski@users.noreply.github.com>
2024-12-05 19:05:53 +0100

    Fixed navbar mobile version

--- client/src/presentation/pages/home-page/index.tsx
+++ client/src/presentation/pages/home-page/index.tsx
@@ -1,12 +1,14 @@
 import styles from "HomePage.module.css";
 import HeroSection from "./sections/hero-section";
 import SetMetaInfo from "../../../infra/utility/SetMetaInfo";
+import WhatWeDoSection from "./sections/hero-section/what_we_do_section";
 
 const HomePage = ():JSX.Element => {
     return (
     <>
         <SetMetaInfo title={"Home Page"} description={"Home page"} /> 
     <HeroSection />
+    <WhatWeDoSection/>
     </>
     )
 }
