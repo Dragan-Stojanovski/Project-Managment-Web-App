846d13088d502ff7a1029f7462c0b05c1d985963
Stefan Parch <sskined@gmail.com>
2024-12-05 21:56:50 +0100

    map init and import

--- client/src/presentation/components/nav-bar/index.tsx
+++ client/src/presentation/components/nav-bar/index.tsx
@@ -1,52 +1,68 @@
-import { Link } from 'react-router-dom';
-import styles from './NavBar.module.css';
+import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
 import { IoMdMenu } from "react-icons/io";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 
 const NavBar = (): JSX.Element => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-    const [isDropdownVisible, setIsDropdownVisible] = useState(isMobile ? false : true); 
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+	const [isDropdownVisible, setIsDropdownVisible] = useState(
+		isMobile ? false : true
+	);
 
-  const navItems = [
-    { label: "What's New?", path: '/news' },
-    { label: "Features", path: '/features' },
-    { label: "Sign In", path: '/signin' }
-  ];
+	const navItems = [
+		{ label: "What's New?", path: "/news" },
+		{ label: "Features", path: "/features" },
+		{ label: "Sign In", path: "/signin" },
+		{ label: "Career Map", path: "/map" },
+		{ label: "Import", path: "/import" },
+	];
 
-  useEffect(() => {
-    const handleResize = () => {
-        setIsMobile(window.innerWidth < 768);
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 768);
+		};
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-        window.removeEventListener("resize", handleResize);
-    };
-}, []);
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  return (
-    <div className={styles.nav_bar__wrapper}>
-     <div className={styles.mobile_logo_overview}>
-      <div className={styles.main_header_logo__place}>
-        <Link to="/"><img src="../../../../assets/images/oman_labor_market_intelligence_analysis_logo.png" alt="Brand Logo" /></Link>
-      </div>
-{    isMobile  &&   <button onClick={() =>setIsDropdownVisible(!isDropdownVisible)}> {isDropdownVisible ? <IoClose /> : <IoMdMenu /> }</button>}
-</div>
-    {isDropdownVisible &&  <div>
-        <ul>
-          {navItems.map((item) => (
-            <li key={item.label}>
-              <Link to={item.path}>{item.label}</Link>
-            </li>
-          ))}
-          <li className={styles.get_started}>
-            <Link to="/signup">Sign up</Link>
-          </li>
-        </ul>
-      </div>}
-    </div>
-  );
+	return (
+		<div className={styles.nav_bar__wrapper}>
+			<div className={styles.mobile_logo_overview}>
+				<div className={styles.main_header_logo__place}>
+					<Link to="/">
+						<img
+							src="../../../../assets/images/oman_labor_market_intelligence_analysis_logo.png"
+							alt="Brand Logo"
+						/>
+					</Link>
+				</div>
+				{isMobile && (
+					<button onClick={() => setIsDropdownVisible(!isDropdownVisible)}>
+						{" "}
+						{isDropdownVisible ? <IoClose /> : <IoMdMenu />}
+					</button>
+				)}
+			</div>
+			{isDropdownVisible && (
+				<div>
+					<ul>
+						{navItems.map((item) => (
+							<li key={item.label}>
+								<Link to={item.path}>{item.label}</Link>
+							</li>
+						))}
+						<li className={styles.get_started}>
+							<Link to="/signup">Sign up</Link>
+						</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NavBar;
---
+++ client/src/presentation/pages/career-map-page/Map.tsx
@@ -0,0 +1,11 @@
+import styles from "./Map.module.css";
+
+const Map = (): JSX.Element => {
+	return (
+		<div className={styles.container}>
+			<h1>Career Map</h1>
+		</div>
+	);
+};
+
+export default Map;
