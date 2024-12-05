611c5e5e4eaf8649d405f1932bc438fc432de3ea
Sava Stevanovic <sava.stev1999@gmail.com>
2024-12-05 21:02:09 +0100

    import

--- server/src/router.js
+++ server/src/router.js
@@ -1,12 +1,16 @@
 
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const {register,login} = require('./controllers/user/authController')
-const {getUserOwnData} = require('./controllers/user/userController')
+const {register,login} = require('./controllers/user/authController');
+const {getUserOwnData} = require('./controllers/user/userController');
 const authenticateToken = require("./services/middlewares/authenticateToken");
+const {jobImport} = require("./controllers/job/jobController");
+const upload = multer({ dest: "uploads/" });
 
 router.post('/register', register)
 router.post('/login', login)
 router.get('/userown',authenticateToken, getUserOwnData)
+router.post('/import', upload.single('file'), jobImport);
 
 module.exports = router;
