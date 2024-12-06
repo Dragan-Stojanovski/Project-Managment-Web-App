f0fd63b41226b5a469d1bd0e8cf2f827fd068bd4
Stefan Parch <sskined@gmail.com>
2024-12-06 23:23:29 +0100

    get job titles

--- server/src/controllers/job-roles/jobRoles.js
+++ server/src/controllers/job-roles/jobRoles.js
@@ -5,10 +5,23 @@ const Job = require("../../models/jobSchema");
 
 exports.getJobRoles = async (req, res) => {
 	try {
-		const jobRoles = await JobSchema.find({});
+		const jobRoles = await Job.find({});
 		res.status(200).json(jobRoles);
 	} catch (err) {
 		console.error(err);
 		res.status(500).send("Error retrieving job roles");
+	}
+};
+
+exports.getJobTitles = async (req, res) => {
+	console.log("getJobTitles");
+	try {
+		const jobTitles = await Job.find({}, "title");
+		const titlesArray = jobTitles.map((job) => job.title);
+		console.log("titlesArray", titlesArray);
+		res.status(200).json(titlesArray);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Error retrieving job titles");
 	}
 };
